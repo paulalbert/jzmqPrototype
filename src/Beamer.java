0a31872a111190ef4c22c64838f70d6eb33efa42
paulalbert <palbert1@yahoo.com>
2013-05-16 22:17:07 -0600

    Moving java to its own package.

---
+++ src/Beamer.java
@@ -0,0 +1,16 @@
+
+
+/** 
+ * This class will beam a family to another place (or thread).
+ * @author paulalbert
+ */
+public class Beamer {
+    
+    Beamer() {
+        
+    }
+    
+    public final void beam() {
+        
+    }
+}
